1ad85f67f25d2bc5a7f4dbeb7ff0a1a312a3ff0b
dulcet11 <innocentngassa6@gmail.com>
2025-05-24 00:59:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 app.get('/',(req,res)=>{
   res.send('Lanternlink backend is running');
